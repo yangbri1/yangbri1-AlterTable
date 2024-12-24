fc960651b3d93d91ccbc073d51e9150a1c5cff5a
Brian Yang <yangbri1@msu.edu>
2024-12-24 00:40:17 +0000

    Used SQL sublanguage DDL to ALTER TABLE ADD a column to existing DB table

---
+++ problem1.sql
@@ -0,0 +1,15 @@
+ALTER TABLE site_user ADD lastname VARCHAR(100);
+-- ALTER TABLE <DB name> ADD <column name> [data type]
+/**
+     *
+     * In problem1.sql, write a SQL statement that adds a "lastname" column to the site_user table, of type
+     * varchar(100).
+     *
+     *   site_user table:
+     *   |    id    |     firstname     |
+     *   --------------------------------
+     *   |1         |'Kevin'            |
+     *   |2         |'Brian'            |
+     *   |3         |'Charles'          |
+     *
+     */
